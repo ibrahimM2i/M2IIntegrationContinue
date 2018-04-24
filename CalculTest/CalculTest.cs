d8d1377857510a5a2f021ba8a21fdde269ea32dd
Administrateur <Administrateur@M2IPORT-9TLQNC2>
2018-04-24 11:01:52 +0200

    test

---
+++ CalculTest/CalculTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using IntegrationContinue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculTest
+{
+	[TestClass]
+	public class CalculTest
+	{
+		[TestMethod]
+		public void TestValue()
+		{
+			string str = "42";
+			var Myint = Calcul.GetInt(str);
+
+			Assert.AreEqual(42, Myint);
+		}
+
+		[TestMethod]
+		public void TestType()
+		{
+			string str = "42";
+			var Myint = Calcul.GetInt(str);
+
+			Assert.IsInstanceOfType( Myint,typeof(int));
+		}
+	}
+}
---
+++ CalculTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a7cfbed-c3d5-40a9-ad65-e806fdfd1dcc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
