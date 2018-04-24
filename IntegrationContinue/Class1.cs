6938083db4d0290004c2313fe466209fef027f51
Administrateur <Administrateur@M2IPORT-9TLQNC2>
2018-04-24 10:15:08 +0200

    erreur

---
+++ IntegrationContinue/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationContinue
+{
+    public class Calcul
+    {
+		public static int GetInt(string str )
+		{
+			return Int32.Parse(str) + 1;
+		}
+    }
+}
